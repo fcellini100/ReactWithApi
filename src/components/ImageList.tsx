3481ee10f8599b9457467caa6f27be3d5347145e
Fernando Cellini <fernandocellini100@gmail.com>
2024-01-03 18:46:20 -0300

    changes on layout

--- src/components/ImageList.tsx
+++ src/components/ImageList.tsx
@@ -1,11 +1,11 @@
 const ImageList = ({ images }: { images: string[] }) => {
   const imageList = images.map((image, index) => (
-    <div key={index}>
-      <img className="object-cover w-full h-auto" src={image}></img>
+    <div key={index} className="aspect-w-3 aspect-h-4 w-full overflow-hidden">
+      <img className="object-cover w-full h-full cursor-pointer" src={image}></img>
     </div>
   ));
 
-  return <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">{imageList}</div>;
+  return <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-4">{imageList}</div>;
 };
 
 export default ImageList;
