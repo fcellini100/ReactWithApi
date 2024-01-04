048cfbc99478dffc9eeba1c9ae2ec3c548edf26a
Fernando Cellini <fernandocellini100@gmail.com>
2024-01-04 18:29:08 -0300

    added html attr name to select

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -21,6 +21,7 @@ const Dropdown: React.FC = () => {
       <select
         value={selected}
         onChange={handleChange}
+        name="image-type"
         className="w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md"
       >
         {Object.values(ImageOption).map((option) => (
